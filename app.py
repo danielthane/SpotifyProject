9b9e28191c90c044e610bd84f174de2a37c2c996
danielthane <dthane8294@hotmail.co.uk>
2023-07-02 21:42:04 +0100

    Updates layout etc

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import time
 import os
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyOAuth
+import matplotlib.pyplot as plt
 
 
 app = Flask(__name__)
@@ -39,14 +40,22 @@ def get_info():
         print("User not logged in")
         redirect(url_for("login", _external=False))
     sp = spotipy.Spotify(auth=token_info['access_token'])
+    current_user = get_user(sp)
     songs, popularity_avg = get_songs(sp)
     artists = get_top_artists(sp)
+    top_artist = songs[0]
     genres = get_genres(sp)
+    genres_list = [i for i in genres]
+    top_genre = next(iter(genres))
     return render_template('test.html', 
+                           current_user=current_user,
                            songs = songs, 
                            popularity=popularity_avg,
                            artists=artists, 
-                           genres=genres)
+                           genres=genres,
+                           genres_list=genres_list,
+                           top_genre=top_genre,
+                           top_artist = top_artist)
 
 
     
@@ -63,7 +72,7 @@ def get_token():
     
     return token_info
 
-scope = "user-library-read user-read-recently-played"
+scope = "user-library-read user-read-recently-played user-top-read"
 def create_spotify_oauth():
     return SpotifyOAuth(client_id=os.getenv("SPOTIPY_CLIENT_ID"), 
                         client_secret=os.getenv("SPOTIPY_CLIENT_SECRET"), 
@@ -72,23 +81,26 @@ def create_spotify_oauth():
 
 
 def get_songs(sp):
-    items = sp.current_user_recently_played(limit=10)['items']
+    items = sp.current_user_top_tracks(limit=6)['items']
     songs = []
     pop_total = 0
     for i in range(len(items)):
         song = {}
-        name = items[i]['track']['name']
-        img_link = (items[i]['track']['album']['images'][2]['url'])
+        name = items[i]['name']
+        img_link = items[i]['album']['images'][2]['url']
         song['title'] = name
         song['image'] = img_link
+        if i == 0:
+            song['big_image'] = items[i]['album']['images'][0]['url']
+        song['artist'] = items[i]['artists'][0]['name']
         songs.append(song)
-        pop_total += items[i]['track']['popularity']
+        pop_total += items[i]['popularity']
     popularity_avg = round(pop_total / len(items)) 
     return songs, popularity_avg
 
 def get_top_artists(sp):
     artists = []
-    items = sp.current_user_top_artists(limit=10, time_range="long_term")['items']
+    items = sp.current_user_top_artists(limit=5, time_range="long_term")['items']
     for i in range(len(items)):
         artist = {}
         artist['name'] = items[i]['name']
@@ -98,7 +110,7 @@ def get_top_artists(sp):
 
 def get_genres(sp):
     all_genres = []
-    items = sp.current_user_top_artists(limit=10, time_range="long_term")['items']
+    items = sp.current_user_top_artists(limit=50, time_range="short_term")['items']
     for i in range(len(items)):
         for genre in items[i]['genres']:
             all_genres.append(genre)
@@ -110,10 +122,28 @@ def get_genres(sp):
             genres[genre] += 1
     sorted_genres = sorted(genres.items(), key=lambda x:x[1], reverse=True)
     sorted_genres = dict(sorted_genres)
-    print(sorted_genres)
     return sorted_genres
 
+def get_user(sp):
+    current_user={}
+    user = sp.current_user()
+    current_user['name'] = user['display_name'].split()[0]
+    current_user['image'] = user['images'][0]['url']
+    current_user['followers'] = user['followers']['total']
+    return current_user
 
+# def get_genres_plot(genres):
+#     labels =[]
+#     sizes =[]
+
+#     for genre,value in genres.items():
+#         if value > 1:
+#             labels.append(genre)
+#             sizes.append(value)
+    
+#     plt.pie(sizes, labels=labels)
+#     plt.axis('equal')
+#     plt.show()
 
 
 
